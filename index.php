53ffb6e96ff485f2ab57c4a52a25b144329b8cf3
Tim <godehi@qq.com>
2020-09-02 17:54:19 +0800

    init

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Create by PhpStorm
- * User: Tim
- * Date: 2020-09-02
- * Time: 17:24
- */
-echo 123;
---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Create by PhpStorm
+ * User: Tim
+ * Date: 2020-09-02
+ * Time: 17:53
+ */
+
+namespace Godehi;
+
+
+class Test
+{
+    public function test()
+    {
+        echo 'test';
+    }
+}
